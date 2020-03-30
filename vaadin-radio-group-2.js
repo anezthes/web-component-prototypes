6617336b110bcf0508a77c09df97f77036313452
Joacim Päivärinne <joacim@vaadin.com>
2020-03-30 10:41:24 +0300

    Updates!

--- vaadin-radio-group-2.js
+++ vaadin-radio-group-2.js
@@ -1,42 +1,36 @@
-import {LitElement, html, css, property} from 'lit-element';
-import {vaadinRadioGroupStyles} from './vaadin-radio-group-styles.js';
+import { LitElement, html, css, property } from 'lit-element';
+import { vaadinRadioGroupStyles } from './vaadin-radio-group-styles.js';
 
-class VaadinRadioGroup extends LitElement {
-  static get properties() {
-    return {
-      legend: {type: String},
-      options: {type: Number}
-    };
-  }
+class VaadinRadioGroup2 extends LitElement {
+  static get properties() { return {
+    legend: { type: String },
+    options: { type: Array }
+  };}
   constructor() {
     super();
     this.legend = '';
-    this.options = 0;
+    this.options = [];
   }
   static get styles() {
-    return [vaadinRadioGroupStyles, css``];
+    return [
+      vaadinRadioGroupStyles,
+      css``
+    ]
   }
   render() {
     return html`
       <fieldset>
         <legend>${this.legend}</legend>
-        ${this._options.map(
-          (option) => html`
-            <label>
-              <input type="radio" name="option" />
-              <slot name="option-${i}"></slot>
-            </label>
-          `
-        )}
+        ${this.options.map((option, i) => html`
+          <label>
+            <input type="radio" name="option">
+            <span>
+              <slot name="option-${i + 1}"></slot>
+            </span>
+          </label>
+        `)}
       </fieldset>
     `;
-  }
-
-  firstUpdated() {
-    super.firstUpdated();
-    for (let i = 0; i < this.options; i++) {
-      this._options.push(i);
-    }
   }
 }
 customElements.define('vaadin-radio-group-2', VaadinRadioGroup2);
