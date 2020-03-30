a234c45aa9d6544249df47fe56534904395c0129
Nii Yeboah <nii@vaadin.com>
2020-03-30 10:26:37 +0300

    Add options prop

--- vaadin-radio-group-2.js
+++ vaadin-radio-group-2.js
@@ -1,37 +1,42 @@
-import { LitElement, html, css, property } from 'lit-element';
-import { vaadinRadioGroupStyles } from './vaadin-radio-group-styles.js';
+import {LitElement, html, css, property} from 'lit-element';
+import {vaadinRadioGroupStyles} from './vaadin-radio-group-styles.js';
 
 class VaadinRadioGroup extends LitElement {
-  static get properties() { return {
-    legend: { type: String },
-    options: { type: Number }
-  };}
+  static get properties() {
+    return {
+      legend: {type: String},
+      options: {type: Number}
+    };
+  }
   constructor() {
     super();
     this.legend = '';
     this.options = 0;
   }
   static get styles() {
-    return [
-      vaadinRadioGroupStyles,
-      css``
-    ]
+    return [vaadinRadioGroupStyles, css``];
   }
   render() {
     return html`
       <fieldset>
         <legend>${this.legend}</legend>
-        ${this.options.forEach((option, i) => html`
-          <label>
-            <input type="radio" name="option">
-            <slot name="option-${i}"></slot>
-          </label>
-        `)}
+        ${this._options.map(
+          (option) => html`
+            <label>
+              <input type="radio" name="option" />
+              <slot name="option-${i}"></slot>
+            </label>
+          `
+        )}
       </fieldset>
     `;
   }
+
   firstUpdated() {
-    _options = 
+    super.firstUpdated();
+    for (let i = 0; i < this.options; i++) {
+      this._options.push(i);
+    }
   }
 }
 customElements.define('vaadin-radio-group-2', VaadinRadioGroup2);
